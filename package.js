3b2f72502bdd1da2f39b4a6847cc7cd529767e25
Michael Lazarski <michael@lazarski.me>
2014-09-15 00:32:41 +0200

    init files

---
+++ package.js
@@ -0,0 +1,13 @@
+/* Information about this package */
+Package.describe({
+  summary: "A CSS Framework for Meteor",
+  version: "0.0.1",
+  name: "ganymed:base",
+  git: "https://github.com/something/something.git",
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.imply('templating');
+  api.addFiles(['libs/normalize.css', 'base.css', 'grid.css', 'responsiveGrid.css'], 'client');
+});
